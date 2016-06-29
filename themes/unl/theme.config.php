08b7677e6e9d8a30f4511056dffbd54068389896
srickel1 <srickel1@unl.edu>
2016-06-29 11:06:58 -0500

    extends bootprint3 citation helper factory

---
+++ themes/unl/theme.config.php
@@ -0,0 +1,9 @@
+<?php
+return array(
+    'extends' => 'bootprint3',
+   'helpers' => array(
+        'factories' => array(
+            'citation' => 'VufindUNL\View\Helper\Root\Factory::getCitation'
+	)
+    )
+);
